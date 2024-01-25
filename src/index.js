a7dad9345a36f48c10d81452d9d62fee4ff14435
Gideon Ogot Junior <juniorgideon99@gmail.com>
2024-01-25 16:13:12 +0300

    mini project done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    
+    const taskInput = document.getElementById("new-task-description");
+    const taskValue = taskInput.value;
+
+    
+    const newTask = document.createElement("li");
+    newTask.textContent = taskValue;
+
+   
+    taskList.appendChild(newTask);
+
+    
+    taskInput.value = "";
+  });
 });
